5a5d9e862f33cc4e92618e28762a6d3a2b9d8fcc
Luiz Mantuano <luguto23@hotmail.com>
2023-12-20 14:23:13 -0300

    feat: "Navigation added"

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
+import { View, Text } from 'react-native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import UserList from './screens/UserList';
+import UserForm from './screens/UserForm';
+import { Button, Icon } from '@rneui/base';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='UserList' screenOptions={screenOptions}>
+        <Stack.Screen
+          name='UserList'
+          component={UserList}
+          options={({ navigation }) => {
+            return {
+              title: 'Lista de Usuários',
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate('UserForm')}
+                  type='clear'
+                  icon={<Icon name='add' size={25} color='#fff' />}
+                />
+              ),
+            };
+          }}
+        />
+        <Stack.Screen
+          name='UserForm'
+          component={UserForm}
+          options={{ title: 'Formulário de Usuários' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e',
   },
-});
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
