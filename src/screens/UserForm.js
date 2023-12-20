af788b5952b1b53fcda5e7ab6d7df5462b3ccc38
Luiz Mantuano <luguto23@hotmail.com>
2023-12-20 14:24:00 -0300

    feat: "Created navigation screens"

---
+++ src/screens/UserForm.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UserForm = () => {
+  return (
+    <View>
+      <Text>UserForm</Text>
+    </View>
+  )
+}
+
+export default UserForm
