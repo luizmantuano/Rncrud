9f1172ef84cce27b1f683e5f6753b078bc5959cd
Luiz Mantuano <luguto23@hotmail.com>
2023-12-20 14:37:27 -0300

    feat: "User list created"

---
+++ src/data/users.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    name: 'João Silva',
+    email: 'josil@gmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_1280.png',
+  },
+  {
+    id: 2,
+    name: 'Mariana Aguiar',
+    email: 'mariaguiar@gmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_1280.png',
+  },
+  {
+    id: 3,
+    name: 'Julio Ferreira',
+    email: 'ferreira_ju@gmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_1280.png',
+  },
+  {
+    id: 4,
+    name: 'Rafael Oliveira',
+    email: 'faeloli@gmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/02/14/11/male-306408_1280.png',
+  },
+];
