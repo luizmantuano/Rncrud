36f5cfd89d259c19dce1b43f85df29c2e187b428
Luiz Mantuano <luguto23@hotmail.com>
2023-12-21 13:21:45 -0300

    feat: "Adding functions"

--- src/screens/UserList.js
+++ src/screens/UserList.js
@@ -1,12 +1,55 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, FlatList, Alert } from 'react-native';
+import React from 'react';
+import { Avatar, Button, Icon, ListItem } from '@rneui/base';
+import users from '../data/users';
 
-const UserList = () => {
+export default (props) => {
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+      {
+        text: 'Sim',
+        onPress() {
+          console.warn('delete' + user.id);
+        },
+      },
+      {
+        text: 'Não',
+      },
+    ]);
+  }
+
+  function getUserItem({ item: user }) {
+    return (
+      <ListItem bottomDivider onPress={() => props.navigation.navigate('UserForm')}>
+        <Avatar source={{ uri: user.avatarUrl }} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+        <Button
+          onPress={() => {
+            props.navigation.navigate('UserForm', user);
+          }}
+          type='clear'
+          icon={<Icon name='edit' size={25} color='orange' />}
+        />
+        <Button
+          onPress={() => {
+            confirmUserDeletion(user);
+          }}
+          type='clear'
+          icon={<Icon name='delete' size={25} color='red' />}
+        />
+      </ListItem>
+    );
+  }
   return (
     <View>
-      <Text>UserList</Text>
+      <FlatList
+        keyExtractor={(user) => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
     </View>
-  )
-}
-
-export default UserList
+  );
+};
