5e57f19140477cfb8bbabc0e0413a60ebda28b15
Luiz Mantuano <luguto23@hotmail.com>
2023-12-21 13:51:11 -0300

    feat: "Added form"

--- src/screens/UserForm.js
+++ src/screens/UserForm.js
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
 
-const UserForm = () => {
+export default ({ route, navigation }) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
   return (
-    <View>
-      <Text>UserForm</Text>
-    </View>
-  )
-}
+    <View style={styles.form}>
+      <Text>Name</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(name) => setUser({ ...user, name })}
+        placeholder='Informe o Nome'
+        value={user.name}
+      />
+      <Text>Eamil</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(email) => setUser({ ...user, email })}
+        placeholder='Informe o E-mail'
+        value={user.email}
+      />
+      <Text>URL do Avatar</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(avatarUrl) => setUser({ ...user, avatarUrl })}
+        placeholder='Informe o URL do Avatar'
+        value={user.avatarUrl}
+      />
 
-export default UserForm
+      <Button
+        title='Salvar'
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+});
